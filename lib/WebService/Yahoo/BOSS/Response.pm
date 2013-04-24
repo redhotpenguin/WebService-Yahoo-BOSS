663c165543d8c8298cf5ae86d6baa2dac7a81f44
Tim Bunce <Tim.Bunce@pobox.com>
2013-04-24 15:47:48 +0100

    Implement PlaceFinder service, with tests. Add/update docs. Bump version.

--- lib/WebService/Yahoo/BOSS/Response.pm
+++ lib/WebService/Yahoo/BOSS/Response.pm
@@ -9,11 +9,13 @@ WebService::Yahoo::BOSS::Response - Response class for Yahoo BOSS searches
 use Moo;
 use JSON::XS ();
 use Data::Dumper;
+use Module::Runtime qw(require_module);
 
 has 'count'        => ( is => 'ro', required => 1 );
-has 'totalresults' => ( is => 'ro', required => 1 );
 has 'start'        => ( is => 'ro', required => 1 );
 has 'results'      => ( is => 'ro', required => 1 );
+has 'totalresults' => ( is => 'ro', required => 0 ); # not provided by all services
+
 
 sub parse {
     my ( $class, $content, $resultclass ) = @_;
@@ -21,9 +23,10 @@ sub parse {
     my $response = JSON::XS::decode_json($content);
 
     my $rc = $response->{bossresponse}->{responsecode};
-
-    die "Boss response contains error: " . Dumper($response)
+    die "Boss response error $rc: " . Dumper($response)
         unless $rc == 200;
+
+    require_module($resultclass);
 
     my $results = $resultclass->parse($response->{bossresponse});
 
